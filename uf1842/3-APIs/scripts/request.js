d79ed32017432ec18b3b9cd81bbc6d6be7bfa9ca
ErebosXYZ <robertmaynou@gmail.com>
2025-04-19 11:25:26 +0200

    done

--- uf1842/3-APIs/scripts/request.js
+++ uf1842/3-APIs/scripts/request.js
@@ -1,5 +1,14 @@
 async function getPuzzle(wordCount) {
-    let hiddenSentence = "Buena Suerte";
+    let hiddenSentence = "Buena suerte!";
+
+    let response = await fetch(`https://puzzle.mead.io/puzzle?wordCount=${wordCount}`);
+
+    let data = await response.json();
+
+    console.log(data);
+
+    hiddenSentence = data.puzzle;
 
     return hiddenSentence;
 }
+
