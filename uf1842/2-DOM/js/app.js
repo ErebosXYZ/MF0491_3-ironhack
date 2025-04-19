2fd4fe0d79441e0ad3dfca27a152e4558959b472
ErebosXYZ <robertmaynou@gmail.com>
2025-04-19 11:02:42 +0200

    done

--- uf1842/2-DOM/js/app.js
+++ uf1842/2-DOM/js/app.js
@@ -1,3 +1,12 @@
 /**
  * ¡Buena suerte! ¡Pasa el mensaje!
  */
+
+const message = document.querySelector("#message");
+const btnSend = document.querySelector("#btn-send");
+const lastMessage = document.querySelector("#last-message");
+
+btnSend.addEventListener("click", () => {
+    lastMessage.textContent = message.value;
+    message.value = "";
+})
